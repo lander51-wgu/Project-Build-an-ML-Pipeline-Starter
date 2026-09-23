7e06245b82b0af46c742a314860860b4a33812e1
Liam <electronalone@gmail.com>
2026-09-23 15:40:22 -0700

    Finished train_random_forest

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -164,7 +164,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     # 2 - A OneHotEncoder() step to encode the variable
     non_ordinal_categorical_preproc = make_pipeline(
         SimpleImputer(strategy="most_frequent"),
-        OneHotEncoder()
+        OneHotEncoder(handle_unknown='ignore')
     )
     ######################################
 
